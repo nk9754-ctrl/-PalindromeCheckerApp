5cd7316385c8a945c1edc754855024242d9e8361
Kandru Nikhil Sai <nk9754@srmist.edu.in>
2026-02-27 08:51:13 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,20 @@
-// File: UseCase9PalindromeCheckerApp.java
+
 
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    // Recursive function to check palindrome
+  
     public static boolean isPalindrome(String str, int start, int end) {
-        // Base condition: if start >= end, it's a palindrome
+       
         if (start >= end) {
             return true;
         }
-        // If mismatch found, not a palindrome
+   
         if (str.charAt(start) != str.charAt(end)) {
             return false;
         }
-        // Recursive call: move inward
+      
         return isPalindrome(str, start + 1, end - 1);
     }
 
@@ -23,10 +23,9 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        // Call recursive palindrome checker
         boolean result = isPalindrome(input, 0, input.length() - 1);
 
-        // Output result
+    
         if (result) {
             System.out.println("The string \"" + input + "\" is a palindrome.");
         } else {
