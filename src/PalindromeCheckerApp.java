d8247772469dcfc7eb71833ae4f32a0b97fc9490
K.Nikhil Sai <nk9754@srmist.edu.in>
2026-02-27 09:14:00 +0530

    UC11:Object-Oriented Palindrome Service

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,35 +1,47 @@
+// File: UseCase11PalindromeCheckerApp.java
+
 import java.util.Scanner;
-import java.util.Deque;
-import java.util.ArrayDeque;
-class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+// Encapsulated PalindromeChecker class
+class PalindromeChecker {
 
-                System.out.print("Enter a string to check: ");
-                String input = scanner.nextLine();
+    // Method to check palindrome
+    public boolean checkPalindrome(String input) {
+        int start = 0;
+        int end = input.length() - 1;
 
-                Deque<Character> deque = new ArrayDeque<>();
-                for (char c : input.toCharArray()) {
-                    deque.add(c);
-                }
-
-                boolean isPalindrome = true;
-                while (deque.size() > 1) {
-                    char front = deque.removeFirst();
-                    char rear = deque.removeLast();
-                    if (front != rear) {
-                        isPalindrome = false;
-                        break;
-                    }
-                }
-
-                if (isPalindrome) {
-                    System.out.println("The string \"" + input + "\" is a palindrome.");
-                } else {
-                    System.out.println("The string \"" + input + "\" is NOT a palindrome.");
-                }
-
-                scanner.close();
+        // Compare characters from both ends
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false; // mismatch found
             }
+            start++;
+            end--;
         }
+        return true; // all matched
+    }
+}
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Create PalindromeChecker object
+        PalindromeChecker checker = new PalindromeChecker();
+
+        // Call the encapsulated method
+        boolean result = checker.checkPalindrome(input);
+
+        // Output result
+        if (result) {
+            System.out.println("The string \"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a palindrome.");
+        }
+
+        sc.close();
+    }
+}
